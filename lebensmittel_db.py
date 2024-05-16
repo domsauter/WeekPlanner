0b49d5853b9e3a0da2e22435a56e461588180169
domsauter <159060903+domsauter@users.noreply.github.com>
2024-05-16 11:37:42 +0200

    Create lebensmittel_db.py

---
+++ lebensmittel_db.py
@@ -0,0 +1,10 @@
+# lebensmittel_db.py
+
+lebensmittel_db = {
+    "Hähnchen": {"Protein": 25, "Kohlenhydrate": 0, "Fett": 3},
+    "Thunfisch": {"Protein": 30, "Kohlenhydrate": 0, "Fett": 1},
+    "Lachs": {"Protein": 20, "Kohlenhydrate": 0, "Fett": 10},
+    "Ei": {"Protein": 6, "Kohlenhydrate": 0, "Fett": 5},
+    "Tofu": {"Protein": 8, "Kohlenhydrate": 2, "Fett": 4},
+    # Weitere Lebensmittel mit ihren Makronährstoffverteilungen hier hinzufügen
+}
